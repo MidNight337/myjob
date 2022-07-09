e14a6ba18e048aee2989ff1a6fe8c61e7f00368f
ZhenyaMidNight <goloveikozhenya@mail.ru>
2022-07-09 14:11:54 +0600

    changingModels

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myjob',
-    'news.apps.NewsConfig',
     'mptt',
 ]
 
@@ -119,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- news/models.py
+++ news/models.py
@@ -1,6 +1,12 @@
+from distutils.command.upload import upload
+from email.mime import image
+from pyexpat import model
 from statistics import mode
+from unicodedata import category
+from venv import create
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -21,4 +27,25 @@ class Category(MPTTModel):
 class Tag(models.Model):
     name = models.CharField(max_length=150)
     slug = models.CharField(max_length=150)
+    
+class Post(models.Model):
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE) 
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to = 'articles/')
+    text = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, 
+                                related_name='post', 
+                                on_delete=models.SET_NULL, 
+                                null = True)
+    tags = models.ManyToManyField(Tag, related_name='post')
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=150)
+    serves = models.CharField(max_length=57)
+    prep_time = models.PositiveIntegerField(default = 0)
+    cook_time = models.PositiveIntegerField(default= 0)
+    ingredients = models.TextField()
+    direction = models.TextField()
+    post = models.ForeignKey(Post, related_name='recipe', on_delete=models.SET_NULL, null=True, blank=True)
     
